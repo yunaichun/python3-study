9cc2a3612395e35e71458970f62241d8c4ffe334
yunaichun <yunaichun@qingting.fm>
2020-04-26 15:14:59 +0800

    python3-带参数装饰器

--- 02.提升篇/装饰器.py
+++ 02.提升篇/装饰器.py
@@ -1,4 +1,4 @@
-# 装饰器
+# 不带参数装饰器
 import time
 def timer(func):
     def wrapper():
@@ -11,5 +11,21 @@ def timer(func):
 def i_can_sleep():
     time.sleep(1)
 i_can_sleep()
+# 装饰器等价于如下写法
 # num = timer(i_can_sleep)
 # num()
+
+
+# 带参数装饰器
+def new_tips(argv):
+    def tips(func):
+        def nei(a, b):
+            print('start %s %s' %(argv, func.__name__))
+            func(a, b)
+            print('end')
+        return nei
+    return tips
+@new_tips('add')
+def add(a, b):
+    print(a+b)
+add(4, 5)
--- README.md
+++ README.md
@@ -5,7 +5,8 @@
 ## 学习参考
 
 - [官方文档](https://docs.python.org/3/)
-- [极客时间](https://time.geekbang.org/course/intro/100008801)
+- [零基础学 Python](https://time.geekbang.org/course/intro/100008801)
+- [Python 核心技术与实战](https://time.geekbang.org/column/intro/100026901)
 - [廖雪峰](https://www.liaoxuefeng.com/wiki/1016959663602400)
 - [菜鸟教程](https://www.runoob.com/python3/python3-tutorial.html)
 - [VScode 环境配置](https://juejin.im/post/5d71bef8f265da03b76b42f6)

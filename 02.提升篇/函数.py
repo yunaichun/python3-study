f73162c641aefd7ab932daa00e2f7ff8e0bb6c8d
yunaichun <yunaichun@qingting.fm>
2020-04-26 15:20:47 +0800

    python3-匿名和内置函数

--- 02.提升篇/函数.py
+++ 02.提升篇/函数.py
@@ -19,24 +19,3 @@ def func():
     print(var1)
 func()
 print(var1)
-
-# lambda 表达式
-def true():return True # 等价于 lambda: True;
-def add(a, b):return a+b # 等价于 lambda a,b: a+b
-def func(item): return item[1] # 等价于 lambda item: item[1]
-adict = {'a': 11, 'b': 22}
-for i in adict.items():
-    print((lambda item: item[1])(i))
-
-# 内建函数: filter()、map()、reduce()、zip()
-a = [1, 2, 3]
-b = [4, 5, 6]
-print(list(filter(lambda x: x> 2, a)))
-print(list(map(lambda x,y: x + y, a, b)))
-from functools import reduce
-print(reduce(lambda x, y: x+y, a, 1))
-for i in zip((1, 2, 3), (4, 5, 6)):
-    print(i)
-adict = {'a': 11, 'b': 22}
-bdict = zip(adict.values(), adict.keys())
-print(dict(bdict))

22439cd47655f68d9f0404e74f79c347c05ad22d
yunaichun <yunaichun@qingting.fm>
2020-04-26 18:43:28 +0800

    python3-字符串

--- 01.基础篇/基本数据结构.py
+++ 01.基础篇/基本数据结构.py
@@ -19,7 +19,7 @@ bandWidth = 100
 radio = 8
 print(bandWidth/radio)
 
-# 字符串操作
+# 四、字符串操作
 chinese_zodiac = '猴鸡狗猪鼠牛虎兔龙蛇马羊'
 # 切片操作
 print(chinese_zodiac[0:4])
@@ -35,3 +35,17 @@ print('狗' not in chinese_zodiac)
 print(chinese_zodiac + 'abcd')
 # 重复操作
 print(chinese_zodiac * 3)
+
+
+# 字符串替换
+s = 'hello'
+# s[0] = 'H' 错误操作
+s = 'H' + s[1:]
+s = s.replace('h', 'H')
+# 表示去掉首尾的 str 字符串; 
+string.strip(s)
+# 表示只去掉开头的 str 字符串; 
+string.lstrip(s)
+# 表示只去掉尾部的 str 字符串。
+string.rstrip(s)
+# 其余方法: split、join

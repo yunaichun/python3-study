cde1fb501bc2096042af09cb369df917094e47d3
yunaichun <yunaichun@qingting.fm>
2020-04-26 15:25:05 +0800

    python3-上下文管理器

--- 02.提升篇/上下文管理器.py
+++ 02.提升篇/上下文管理器.py
@@ -1,8 +1,14 @@
-# fd = open('name.txt')
-# try:
-#     for line in fd
-#     print(line)
-# finally:
-#     fd.close()
+# try...catch...捕获异常
+file = open('name.txt')
+try:
+    for line in file.readlines():
+        print(line)
+except Exception as e:
+    print(e)
+finally:
+    fd.close()
 
-# with open('name.txt') as f:
+# 上下文管理器 with 可以不用写 try...catch...
+with open('name.txt') as file:
+    for line in file.readlines():
+        print(line)

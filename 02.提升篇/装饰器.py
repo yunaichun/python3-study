fb3c7546ac067cbda7ac28702d08879bc137b91a
yunaichun <yunaichun@qingting.fm>
2020-04-30 19:58:27 +0800

    python3-装饰器

--- 02.提升篇/装饰器.py
+++ 02.提升篇/装饰器.py
@@ -1,4 +1,4 @@
-# 不带参数装饰器
+# 一、不带参数装饰器
 import time
 def timer(func):
     def wrapper():
@@ -16,7 +16,7 @@ i_can_sleep()
 # num()
 
 
-# 带参数装饰器
+# 二、带参数装饰器
 def new_tips(argv):
     def tips(func):
         def nei(a, b):
@@ -29,3 +29,39 @@ def new_tips(argv):
 def add(a, b):
     print(a+b)
 add(4, 5)
+
+
+# 三、类装饰器
+class Count:
+    def __init__(self, func):
+        self.func = func
+        self.num_calls = 0
+ 
+    def __call__(self, *args, **kwargs):
+        self.num_calls += 1
+        print('num of calls is: {}'.format(self.num_calls))
+        return self.func(*args, **kwargs)
+
+@Count
+def example():
+    print("hello world")
+
+example()
+example()
+
+
+# 四、应用-身份认证登录拦截、日志记录、输入合理性检查
+import functools
+def authenticate(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        request = args[0]
+        if check_user_logged_in(request): # 如果用户处于登录状态
+            return func(*args, **kwargs) # 执行函数 post_comment() 
+        else:
+            raise Exception('Authentication failed')
+    return wrapper
+
+@authenticate
+def post_comment(request):
+    print(111)

fbbfb921cfd1ac5490d51c3748424912ccc0d635
yunaichun <yunaichun@qingting.fm>
2020-08-11 20:29:43 +0800

    python3 日期时间格式化

--- 03基础库/日期和时间.py
+++ 03基础库/日期和时间.py
@@ -6,8 +6,21 @@ print(time.strftime('%Y-%m-%d %H:%M:%S'))
 
 import datetime
 print(datetime.datetime.now())
-newtime = datetime.timedelta(minutes=10)
-print(newtime)
+interval = datetime.timedelta(minutes=10)
+print(interval)
 one_day = datetime.datetime(2018, 5, 27)
-new_date = datetime.timedelta(days=10)
-print(one_day + new_date)
+interval = datetime.timedelta(days=10)
+print(one_day + interval)
+# datetime 格式化 https://blog.csdn.net/shomy_liu/article/details/44141483
+# 含有毫秒
+format_date1 = '%Y-%m-%dT%H:%M:%S.%fZ'
+prev_time = datetime.datetime.strptime('2020-06-19T04:05:16.77Z', format_date1)
+interval = datetime.timedelta(days=int(0))
+modified_time = (prev_time + interval).strftime(format_date1)
+print(modified_time)
+
+# 不含有毫秒
+format_date2 = '%Y-%m-%dT%H:%M:%SZ'
+init_time = datetime.datetime.strptime('2019-07-29T17:57:29Z', format_date2).strftime(format_date2)
+now_time = datetime.datetime.now().strftime(format_date2)
+print(init_time > now_time)
